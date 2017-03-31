eb652f4b2972258b3d04aaf385b4b4d5946212b1
Owen Jose <owen_jose@hotmail.com>
2017-03-31 15:40:44 +0200

    serch and pop-up workig acceptable

--- main.js
+++ main.js
@@ -87,168 +87,6 @@ const PocketMonsters = (function() {
 (function() {
 	
 	let pocketMonsters = PocketMonsters.allPokemons;
-
-	const gettingNames = () => {
-		let names = [];
-		for(let i = 0; i < pocketMonsters.length; i++) {
-			for(let j = 0; j < pocketMonsters[i].results.length; j++) {
-				names.push(pocketMonsters[i].results[j].name);
-			}
-		}
-		return names;
-	};
-	function comperingString(value) {
-		for(let i = 0; i < pocketMonsters.length; i++) {
-			for(let j = 0; j < pocketMonsters[i].results.length; j++) {
-				if (value === pocketMonsters[i].results[j].name){
-					return true;
-				}
-				else {
-					return false;
-				}
-			}
-		}
-	}
-
-	$("#hamburger").on("click", function(){
-		$("#menu").slideToggle("slow");
-		console.log(gettingNames());
-		console.log();
-	});
-
-	$("#search").on("click", function(){
-		$("#searchInput").slideToggle().focus().val("");
-	});
-
-	$("#menu").on("mouseleave", function(){
-		$("#menu").slideUp("fast");
-	});	
-	$("#searchInput").autocomplete({
-		source: gettingNames()
-	});
-
-	$("#pop-up").on("click", function(){
-		$("#pop-up").fadeOut("fast");
-		$("#info-box img").remove();
-	});	
-	// $("ol li:nth-child(2)").on("click", function(){
-	// 	let pocketMonsters = PocketMonsters.getPokeAlfabetisally();
-	// 	// creat a function dont't repeat your self!
-	// 	for (let i = 0; i < pocketMonsters.length; i++) {
-	// 		$("#" + (i+1)).attr("src", pocketMonsters[i].sprites.front_default);
-	// 		console.log(pocketMonsters[i].name);
-	// 	}
-	// 	console.log(pocketMonsters);
-	// });
-
-	// $("ol li:nth-child(3)").on("click", function(){
-	// 	let pocketMonsters = PocketMonsters.getPokeByID();
-	// 	// creat a function dont't repeat your self!
-	// 	for (let i = 0; i < pocketMonsters.length; i++) {
-	// 		$("#" + (i+1)).attr("src", pocketMonsters[i].sprites.front_default);
-	// 		console.log(pocketMonsters[i].id);
-	// 	}
-	// });
-
-	$("#searchInput").keydown(function(e){
-   	// trying to compare the input string some how
-   	// a ain't getting any closer.
-     	let typeList;
-
-		if (e.keyCode == 13) {
-			console.log($("#searchInput").val());
-			if (comperingString($("#searchInput").val()) === true) {
-  				console.log("YES!!");
-  				$("#pop-up").fadeIn();
-  			}
-	 		
-	 		else if (isNaN($("#searchInput").val()) === true) {
-	    		$.ajax({
-					type: "GET",
-					url: "http://pokeapi.co/api/v2/type/" + $("#searchInput").val() + "/",
-					success: function(type){
-						console.log("YES!!" + "success");
-						console.log(type);
-						for (var i = 0; i < type.pokemon.length; i++) {
-							if (type.pokemon[i].pokemon.url > "http://pokeapi.co/api/v2/pokemon/151");{
-								console.log(type.pokemon[i].pokemon.name);
-							}
-						}
-					},
-					error: function(argument) {
-						alert("No Worko");
-					}
-	    		});
-	    	}
-	    else {
-	    	$.ajax({
-				type: "GET",
-				url: "http://pokeapi.co/api/v2/pokemon/" + parseInt($("#searchInput").val()),
-				success: function(pokemon){
-					console.log("success");
-					console.log(pokemon);
-				},
-				error: function(argument) {
-					alert("No Worko");
-				}
-	    	});
-	    	$("#info-box").append('<img src="img/' + parseInt($("#searchInput").val()) + '.png">');
-	    	$("#pop-up").fadeIn();
-	    }	
-	 	$("#searchInput").slideUp();
-	}
-				
-	// 			// my or operator didn't work wihtout the nested if statement
-	// 			// goes in here if the pokemon has more the 1 type, wich for now is usually 2
-	// 			if (pocketMonsters[i].types.length > 1) {
-	//     			// here is were we find the pokemon by name
-	//     			if ($("#searchInput").val().toLowerCase() === pocketMonsters[i].name) {
-	//     				console.log(pocketMonsters[i]);
-	//     				break;
-	//     			}
-	//     			// is were we find the pukemon by id
-	//     			else if ($("#searchInput").val() == pocketMonsters[i].id) {
-	// 					console.log(pocketMonsters[i]);
-	//     				break;
-	// 				}
-	// 				// here i were we find the type of pokemons.
-	// 				else if ($("#searchInput").val() === pocketMonsters[i].types[0].type.name || $("#searchInput").val() === pocketMonsters[i].types[1].type.name) {
-	// 					typeList.push(pocketMonsters[i]);
-	// 					console.log(typeList);
-	// 				}
-	// 			}
-	// 			// in here pokemon only har 1 type.
-	// 			else{
-	// 				if ($("#searchInput").val().toLowerCase() === pocketMonsters[i].name) {
-	//     				console.log(pocketMonsters[i]);
-	//     				break;
-	//     			}
-	//     			// is were we find the pukemon by id
-	//     			else if ($("#searchInput").val() == pocketMonsters[i].id) {
-	// 					console.log(pocketMonsters[i]);
-	//     				break;
-	// 				}
-	// 				// here i were we find the type of pokemons.
-	// 				else if ($("#searchInput").val() === pocketMonsters[i].types[0].type.name) {
-	// 					typeList.push(pocketMonsters[i]);
-	// 					console.log(typeList);
-	// 				}
-	// 			}
-
-	// 		}
-			// Hidding the search field 
-			// $("#searchInput").slideUp();
-			// emptying the array
-			typeList = [];
-	   	// }
-	   	
-	});
-})();
-
-// setting the background to match the pokemon type
-// won't have the time to make this happen....
-(function(){
-
 	const colors = {
 		bug: "#d1fcd7",
 		dragon: "#96755a",
@@ -269,6 +107,167 @@ const PocketMonsters = (function() {
 		dark: "#282828",
 		normal: "#fffdf2"
 	};
+
+	// this is for the Autocomplete
+	const gettingNames = () => {
+		let names = [];
+		for(let i = 0; i < pocketMonsters.length; i++) {
+			for(let j = 0; j < pocketMonsters[i].results.length; j++) {
+				names.push(pocketMonsters[i].results[j].name);
+			}
+		}
+		return names;
+	};
+	// comparing input value to pokemon
+	let comperingString = (value) => {
+		for(let i = 0; i < pocketMonsters.length; i++) {
+			for(let j = 0; j < pocketMonsters[i].results.length; j++) {
+				if (value === pocketMonsters[i].results[j].name){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	let addingInfo = (pokemon) => {
+		$("#searchInput").slideUp();
+		$("#pop-up").fadeIn();
+		$("#pop-up").css({"display": "flex"});
+		$("#info-box").append('<img src="img/' + pokemon.id + '.png">');
+		$("#info-box").append('<h2> #' + pokemon.id + '</h2>');
+		$("#info-box").append('<h3> Name: ' + pokemon.name + '</h2>');
+		$("#info-box").append('<h3> Primery type: ' + pokemon.types[0].type.name + '</h2>');
+		if (pokemon.types.length > 1)
+			$("#info-box").append('<h3> Scondery type: ' + pokemon.types[1].type.name + '</h2>');
+	};
+
+	let removePopUp = () => {
+		$("#pop-up").fadeOut("fast");
+		$("#info-box").empty();
+	};
+
+	$("#pop-up").on("click", function(){
+		$("#pop-up").fadeOut("fast");
+		$("#info-box").empty();
+	});
+	$("#hamburger").on("click", function(){
+		removePopUp();
+		$("#menu").slideToggle("slow");
+	});
+	$("#search").on("click", function(){
+		removePopUp();
+		$("#searchInput").slideToggle().focus().val("");
+	});
+	$("#pokeList img").on("click", function(){
+		removePopUp();
+		$.ajax({
+			type: "GET",
+			url: "http://pokeapi.co/api/v2/pokemon/" + this.id,
+			success: function(pokemon){
+				addingInfo(pokemon);
+			},
+			error: function(argument) {
+				alert("No Worko");
+			}
+	  	});
+	});	
+
+	$("#menu").on("mouseleave", function(){
+		$("#menu").slideUp("fast");
+	});	
+	$("#searchInput").autocomplete({
+		source: gettingNames()
+	});
+
+	// $("ol li:nth-child(2)").on("click", function(){
+	// 	let pocketMonsters = PocketMonsters.getPokeAlfabetisally();
+	// 	// creat a function dont't repeat your self!
+	// 	for (let i = 0; i < pocketMonsters.length; i++) {
+	// 		$("#" + (i+1)).attr("src", pocketMonsters[i].sprites.front_default);
+	// 		console.log(pocketMonsters[i].name);
+	// 	}
+	// 	console.log(pocketMonsters);
+	// });
+
+	// $("ol li:nth-child(3)").on("click", function(){
+	// 	let pocketMonsters = PocketMonsters.getPokeByID();
+	// 	// creat a function dont't repeat your self!
+	// 	for (let i = 0; i < pocketMonsters.length; i++) {
+	// 		$("#" + (i+1)).attr("src", pocketMonsters[i].sprites.front_default);
+	// 		console.log(pocketMonsters[i].id);
+	// 	}
+	// });
+
+	$("#searchInput").keydown(function(e){
+		// when pressing enter on the keyboard
+		if (e.keyCode == 13) {
+			// compare the string input with the stored pokmon names an returns true
+			if (comperingString($("#searchInput").val()) === true) {
+  				$.ajax({
+					type: "GET",
+					url: "http://pokeapi.co/api/v2/pokemon/" + $("#searchInput").val() + "/",
+					success: function(pokemon){
+						addingInfo(pokemon);
+					},
+					error: function(argument) {
+						alert("somthing went wrong!, let's try again");
+						$("#searchInput").val("");
+					}
+	    		});
+  				// console.log("YES!!");
+  				// $("#info-box").append('<img src="img/' + parseInt($("#searchInput").val()) + '.png">');
+  				// $("#pop-up").fadeIn();
+  				// $("#searchInput").slideUp();
+  			}
+	 	
+		 	else if (isNaN($("#searchInput").val()) === true) {
+	    		$.ajax({
+					type: "GET",
+					url: "http://pokeapi.co/api/v2/type/" + $("#searchInput").val() + "/",
+					success: function(type){
+						console.log("YES!!" + "success");
+						console.log(type);
+						for (var i = 0; i < type.pokemon.length; i++) {
+							console.log(type.pokemon[i].pokemon.name);
+							$("#pop-up").fadeIn();
+		    				$("#searchInput").slideUp();
+						}
+					},
+					error: function(argument) {
+						alert($("#searchInput").val() + " is not a pokemon, nor is it a pokemon type, not even an ID-number");
+						$("#searchInput").val("");
+					}
+	    		});
+	    		// $("#info-box").append('<img src="img/' + parseInt($("#searchInput").val()) + '.png">');
+	    	}
+		    else{
+		    	if (parseInt($("#searchInput").val()) > 151){
+		    		alert("Sorry only dealing first gen pokemon");
+		    	}
+			    	
+		    	else{	
+			    	$.ajax({
+						type: "GET",
+						url: "http://pokeapi.co/api/v2/pokemon/" + parseInt($("#searchInput").val()),
+						success: function(pokemon){
+							addingInfo(pokemon);
+						},
+						error: function(argument) {
+							alert("No Worko");
+						}
+			    	});
+			    }	
+		    }
+		}
+	});
+})();
+
+// setting the background to match the pokemon type
+// won't have the time to make this happen....
+(function(){
+
+	
 	$("");
 
 	// css background pokemons with 2 types $("img").css(background: linear-gradient(-45deg, red, yellow);)
